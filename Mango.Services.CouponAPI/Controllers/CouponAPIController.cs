c3a0df95489c5eb0ff27451985d43deff13c1522
ddlibaneo <daniel.libaneo@gmail.com>
2023-05-31 23:31:54 -0300

    add controller and get, add responsedto

---
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,57 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private ReponseDto _response;
+
+        public CouponAPIController(AppDbContext db)
+        {
+            _db = db;
+            _response = new ReponseDto();
+        }
+
+        [HttpGet]
+        public ReponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                _response.Result = objList;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ReponseDto Get(int id)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(c => c.CouponId == id);
+                _response.Result = obj;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+    }
+}
---
+++ Mango.Services.CouponAPI/Models/Dto/ReponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Services.CouponAPI.Models.Dto
+{
+    public class ReponseDto
+    {
+        public object? Result { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public string Message { get; set; } = "";
+
+    }
+}
