db4dded630c4a2727fc3db3856dfe5541db1356c
ddlibaneo <daniel.libaneo@gmail.com>
2023-06-01 21:41:06 -0300

    add mvc web app

---
+++ Mango.Web/Utility/SD.cs
@@ -0,0 +1,16 @@
+﻿namespace Mango.Web.Utility
+{
+    /// <summary>
+    /// Static Details
+    /// </summary>
+    public class SD
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
