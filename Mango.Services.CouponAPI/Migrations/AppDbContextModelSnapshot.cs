f6952fb997d084cf92f3a876d9eebab8be4c609b
ddlibaneo <daniel.libaneo@gmail.com>
2023-06-07 00:45:57 -0300

    add identity

--- Mango.Services.CouponAPI/Migrations/AppDbContextModelSnapshot.cs
+++ Mango.Services.CouponAPI/Migrations/AppDbContextModelSnapshot.cs
@@ -41,7 +41,7 @@ namespace Mango.Services.CouponAPI.Migrations
 
                     b.HasKey("CouponId");
 
-                    b.ToTable("Coupons");
+                    b.ToTable("Coupons", (string)null);
 
                     b.HasData(
                         new
