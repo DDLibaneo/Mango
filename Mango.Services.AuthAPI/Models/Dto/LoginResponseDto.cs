0c8ad85ecacb553d056861002901068fe010970f
ddlibaneo <daniel.libaneo@gmail.com>
2023-06-07 12:58:55 -0300

    add auth dtos

---
+++ Mango.Services.AuthAPI/Models/Dto/LoginResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Mango.Services.AuthAPI.Models.Dto
+{
+    public class LoginResponseDto
+    {
+        public UserDto User { get; set; }
+
+        public string Token { get; set; }
+    }
+}
---
+++ Mango.Services.AuthAPI/Models/Dto/UserDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Mango.Services.AuthAPI.Models.Dto
+{
+    public class UserDto
+    {
+        public string Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
