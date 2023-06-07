710f6f3511c6586b1f4b0ec576d4dc86d15810ca
ddlibaneo <daniel.libaneo@gmail.com>
2023-06-07 15:15:50 -0300

    add authservice, register method

---
+++ Mango.Services.AuthAPI/Service/AuthService.cs
@@ -0,0 +1,65 @@
+﻿using Mango.Services.AuthAPI.Data;
+using Mango.Services.AuthAPI.Models;
+using Mango.Services.AuthAPI.Models.Dto;
+using Mango.Services.AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Identity;
+
+namespace Mango.Services.AuthAPI.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly AppDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthService(AppDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<UserDto> Register(RegistrationRequestDto registrationRequestDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = registrationRequestDto.Name,
+                Email = registrationRequestDto.Email,
+                NormalizedEmail = registrationRequestDto.Email.ToUpper(),
+                Name = registrationRequestDto.Name,
+                PhoneNumber = registrationRequestDto.PhoneNumber
+            };
+
+            try
+            {
+                var result = await _userManager.CreateAsync(user, registrationRequestDto.Password);
+
+                if (result.Succeeded)
+                {
+                    var userToReturn = _db.ApplicationUsers.First(u => u.Email.ToUpper() == registrationRequestDto.Email.ToUpper());
+
+                    var dto = new UserDto
+                    {
+                        Email = userToReturn.Email,
+                        Id = userToReturn.Id,
+                        Name = userToReturn.Name,
+                        PhoneNumber = userToReturn.PhoneNumber
+                    };
+
+                    return dto;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return new UserDto();
+        }
+    }
+}
