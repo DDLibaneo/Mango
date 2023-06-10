f524d2271cfd338624de69cf4fcad28687132d78
ddlibaneo <daniel.libaneo@gmail.com>
2023-06-09 21:43:57 -0300

    implement controller and views

---
+++ Mango.Web/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            var loginRequestDto = new LoginRequestDto();
+
+            return View(loginRequestDto);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult LogOut()
+        {
+            return View();
+        }
+    }
+}
