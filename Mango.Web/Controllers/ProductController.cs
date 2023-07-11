d67e75ada276235759f9e65d5ec6dbd3a5918906
Daniel Libaneo <daniel.libaneo@datasystem.com.br>
2023-07-11 18:52:00 -0300

    wip web

---
+++ Mango.Web/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Mango.Web.Services.IService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> ProductIndex()
+        {
+
+        }
+    }
+}
---
+++ Mango.Web/Services/IService/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Services.IService
+{
+    public interface IProductService
+    {
+        Task<ResponseDto?> GetProductByIdAsync(int id);
+
+        Task<ResponseDto?> GetAllProductsAsync();
+
+        Task<ResponseDto?> CreateProductAsync(ProductDto productDto);
+
+        Task<ResponseDto?> UpdateProductAsync(ProductDto productDto);
+
+        Task<ResponseDto?> DeleteProductAsync(int id);
+    }
+}
