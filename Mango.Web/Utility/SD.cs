655ddff13a7ba97fb3be1851e1f222cd8a842c1d
Daniel Libaneo <daniel.libaneo@datasystem.com.br>
2023-07-12 11:23:13 -0300

    add ProductService on web

--- Mango.Web/Utility/SD.cs
+++ Mango.Web/Utility/SD.cs
@@ -9,6 +9,8 @@
 
         public static string AuthAPIBase { get; set; }
 
+        public static string ProductAPIBase { get; set; }
+
         public const string RoleAdmin = "ADMIN";
 
         public const string RoleCustomer = "CUSTOMER";
