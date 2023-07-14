08821473e82e48ee48dc6896eabf277d1c9fa10a
Daniel Libaneo <daniel.libaneo@datasystem.com.br>
2023-07-14 12:12:51 -0300

    add shopping cart service api

---
+++ Mango.Services.ShoppingCartAPI/Models/Dto/CartDetailsDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Mango.Services.ShoppingCartAPI.Models.Dto
+{
+    public class CartDetailsDto
+    {
+        public int CartDetailsId { get; set; }
+
+        public int CartHeaderId { get; set; }
+
+        public CartHeader CartHeader { get; set; }
+
+        public int ProductId { get; set; }
+
+        public ProductDto Product { get; set; }
+
+        public int Count { get; set; }
+    }
+}
